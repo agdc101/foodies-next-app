7d0acbae4d997dc285776d2313c7299e229a9a5d
agdc101 <agdc101@gmail.com>
2024-04-18 08:34:01 +0100

    feat: db query created based on route

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,11 +1,37 @@
-import Link from "next/link";
+import classes from "./page.module.css";
+// import Link from "next/link";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
 
 export default function MealsFeaturePage({ params }) {
+    const meal = getMeal(params.slug);
+
+    //replace newlines with <br /> tags
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
     return (
-        <div>
-            <h1>Meals Feature</h1>
-            <p><Link href="" ></Link></p>
-            <p>Params: {params.slug}</p>
-        </div>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image
+                        src={meal.image}
+                        alt={meal.title}
+                        fill
+                    />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        By <a href={`mailto:${meal.creator_title}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions
+                }} ></p>
+            </main>
+        </>
     );
 }
